1cb83ef67006dc4a726a8c729307bd0fcd172b50
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-02-03 20:10:21 +0530

    Just made a program in Visual studio

---
+++ CarDetailsLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarDetailsLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
