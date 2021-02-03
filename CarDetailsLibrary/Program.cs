576c67da193af509ebe4de9d5106203d13d65725
dhruvp1997 <30743023+dhruvp1997@users.noreply.github.com>
2021-02-03 23:28:53 +0530

    Written a menu and a description.

--- CarDetailsLibrary/Program.cs
+++ CarDetailsLibrary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Channels;
 
 namespace CarDetailsLibrary
 {
@@ -6,7 +7,17 @@ namespace CarDetailsLibrary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\n\n\tCar Details Library");
+            Console.WriteLine("\n\tAn application to see the car details. There are six columns available: company name, model name, " +
+                              "\n\tengine, top speed, year of launch, and approx price. One can add new car in this library by " +
+                              "\n\tfollowing option 1 from the menu. By following option 2, it will list all the records available " +
+                              "\n\tin library. Option 3 will close the application.");
+            Console.Write("\n\n\tChoose any one option from the following:");
+            Console.Write("\n\n\t1. Add a new car details.");
+            Console.Write("\n\t2. To see list of all cars available in library.");
+            Console.Write("\n\t3. To exit from the library.");
+            Console.Write("\n\n\tChoice:");
+            Console.ReadLine();
         }
     }
 }
